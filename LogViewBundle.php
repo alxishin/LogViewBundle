f34b92262f920e7e797907a354ecda7491524257
alxishin <alxishin@gmail.com>
2015-07-30 18:07:02 +0300

    Create LogViewBundle.php

---
+++ LogViewBundle.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * This file is part of the Symfony framework.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+namespace Aishin\LogviewBundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AishinLogviewBundle extends Bundle
+{
+    public function boot()
+    {
+
+    }
+}
